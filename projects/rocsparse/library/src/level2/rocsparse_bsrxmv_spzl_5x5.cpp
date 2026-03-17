e3a00a8281dffeaa683187fd2d71deee37a81e69
kliegeois <kimliegeois@ymail.com>
2026-03-17 17:15:46 -0600

    tmp

--- projects/rocsparse/library/src/level2/rocsparse_bsrxmv_spzl_5x5.cpp
+++ projects/rocsparse/library/src/level2/rocsparse_bsrxmv_spzl_5x5.cpp
@@ -209,8 +209,9 @@ namespace rocsparse
         else
         {
             // Accumulate the row sum for different blocks
-            if(hipThreadIdx_x < BSRDIM * BSRDIM)
-                ; //sdata[hipThreadIdx_x] += sdata[hipThreadIdx_x + BSRDIM * BSRDIM];
+            if((hipThreadIdx_x < BSRDIM * BSRDIM)
+               && (hipThreadIdx_x + BSRDIM * BSRDIM < BSRDIM * BSRDIM * NBLOCKS))
+                sdata[hipThreadIdx_x] += sdata[hipThreadIdx_x + BSRDIM * BSRDIM];
             __threadfence_block();
 
             // Reduce the intra block row sum
