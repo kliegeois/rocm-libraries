922ec51f307c602cd9ecebc8458ba02d5a5b7977
kliegeois <kimliegeois@ymail.com>
2026-04-15 12:09:53 -0600

    fix illegal kernels

--- projects/rocsparse/library/src/reordering/rocsparse_csrcolor.cpp
+++ projects/rocsparse/library/src/reordering/rocsparse_csrcolor.cpp
@@ -1,6 +1,6 @@
 /*! \file */
 /* ************************************************************************
- * Copyright (C) 2021-2025 Advanced Micro Devices, Inc. All rights Reserved.
+ * Copyright (C) 2021-2026 Advanced Micro Devices, Inc. All rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -139,11 +139,6 @@ namespace rocsparse
                 // Get the number of previous non-zero in the row.
                 //
                 const uint64_t count_previous_uncolored = __popcll(wavefront_mask & filter);
-
-                //
-                // Synchronize for cache considerations.
-                //
-                __syncthreads();
 
                 if(predicate)
                 {
